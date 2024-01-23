e0eba7f5456cff946be8f6eb20dba4b7f4d9b528
kpolanczyk <kacper.pol2002@gmail.com>
2024-01-23 11:15:27 +0100

    kolejne zmiany

--- grafyy/Form1.cs
+++ grafyy/Form1.cs
@@ -181,58 +181,6 @@ namespace grafyy
                 MessageBox.Show("Wprowadź poprawny numer wierzchołka.", "Błąd");
             }
         }
-        private List<int> PrzeszukiwanieWszerz(Wierzcholek startowyWierzcholek)
-        {
-            List<int> wyniki = new List<int>();
-            Queue<Wierzcholek> kolejka = new Queue<Wierzcholek>();
-            HashSet<Wierzcholek> odwiedzone = new HashSet<Wierzcholek>();
-
-            kolejka.Enqueue(startowyWierzcholek);
-            odwiedzone.Add(startowyWierzcholek);
-
-            while (kolejka.Count > 0)
-            {
-                Wierzcholek aktualny = kolejka.Dequeue();
-                wyniki.Add(aktualny.Id);
-
-                foreach (Wierzcholek sasiad in aktualny.Nastepniki)
-                {
-                    if (!odwiedzone.Contains(sasiad))
-                    {
-                        kolejka.Enqueue(sasiad);
-                        odwiedzone.Add(sasiad);
-                    }
-                }
-            }
-
-            return wyniki;
-        }
-        private List<int> PrzeszukiwanieWGlab(Wierzcholek startowyWierzcholek)
-        {
-            List<int> wyniki = new List<int>();
-            Stack<Wierzcholek> stos = new Stack<Wierzcholek>();
-            HashSet<Wierzcholek> odwiedzone = new HashSet<Wierzcholek>();
-
-            stos.Push(startowyWierzcholek);
-            odwiedzone.Add(startowyWierzcholek);
-
-            while (stos.Count > 0)
-            {
-                Wierzcholek aktualny = stos.Pop();
-                wyniki.Add(aktualny.Id);
-
-                foreach (Wierzcholek sasiad in aktualny.Nastepniki)
-                {
-                    if (!odwiedzone.Contains(sasiad))
-                    {
-                        stos.Push(sasiad);
-                        odwiedzone.Add(sasiad);
-                    }
-                }
-            }
-
-            return wyniki;
-        }
 
             private void Wynik_SelectedIndexChanged(object sender, EventArgs e)
         {
